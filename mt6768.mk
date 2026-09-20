b92299a942131c9bc51517805da7d497c475d0ea
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2026-09-20 11:27:40 +0600

    mt6768-common: remove unused AGO memory init script
    Remove the AGO script installed outside its imported path and drop its package and import references. Keep zRAM configuration in init.mt6768.rc before swapon_all, preserving the active swappiness value of 80.

--- mt6768.mk
+++ mt6768.mk
@@ -213,7 +213,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 PRODUCT_PACKAGES += \
     fstab.mt6768 \
     fstab.mt6768.ramdisk \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6768.rc \
